cffdba10c77477ac135db1c79cdf75bcab2a29cc
RushabhaJain <rushabhajain44@gmail.com>
2024-02-10 11:21:22 +0530

    Add uuid package and add /deploy endpoint for deploying github repo

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,14 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(express.json());
 app.use(cors());
+
+app.post("/deploy", (req, res) => {
+  const { repoUrl } = req.body;
+  console.log(repoUrl);
+  res.json({});
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
