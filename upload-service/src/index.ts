c931f76d0ed73eeec277d02d4192d38da88207cc
RushabhaJain <rushabhajain44@gmail.com>
2024-02-10 17:13:37 +0530

    Created mono repo

--- upload-service/src/index.ts
+++ upload-service/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
-import uuid from "uuid";
+import { v4 } from "uuid";
 import simpleGit from "simple-git";
-import { getAllFiles } from "./file";
+import { deleteFolder, getAllFiles } from "./file";
 import path from "path";
 import dotenv from "dotenv";
+import { uploadFile } from "./s3";
+import { publish } from "./messageQueue";
 
 // Load environment variables
 dotenv.config();
@@ -16,7 +18,7 @@ app.use(cors());
 app.post("/deploy", async (req, res) => {
   const { repoUrl } = req.body;
   if (repoUrl != null) {
-    const id = uuid.v4();
+    const id = v4();
     const outputPath = `output/${id}`;
 
     // Clone the repo at repoUrl and put it into outputPath
@@ -24,6 +26,32 @@ app.post("/deploy", async (req, res) => {
 
     // Upload the cloned repo to S3
     const files = await getAllFiles(path.join(__dirname, outputPath));
+
+    const { ACCESS_KEY, SECRET_ACCESS_KEY, BUCKET_NAME, REGION } = process.env;
+
+    const fileUploadPromises = files.map(async (file) => {
+      await uploadFile(
+        ACCESS_KEY as string,
+        SECRET_ACCESS_KEY as string,
+        REGION as string,
+        BUCKET_NAME as string,
+        file,
+        file.slice(path.join(__dirname, "output").length + 1)
+      );
+    });
+
+    await Promise.all(fileUploadPromises);
+
+    console.log("Files successfully uploaded to s3");
+
+    // Add the message to queue
+    await publish({
+      value: id,
+    });
+
+    console.log("Message successfuly published to queue");
+
+    deleteFolder(path.join(__dirname, outputPath));
 
     res.json({
       id,
