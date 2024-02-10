e37d423a62be6580bf1619b252d714946dc5a217
RushabhaJain <rushabhajain44@gmail.com>
2024-02-10 11:30:01 +0530

    add @type/uuid and updated deploy endpoint handler to clone the repo locally

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express from "express";
 import cors from "cors";
+import uuid from "uuid";
+import simpleGit from "simple-git";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/deploy", (req, res) => {
+app.post("/deploy", async (req, res) => {
   const { repoUrl } = req.body;
-  console.log(repoUrl);
-  res.json({});
+  if (repoUrl != null) {
+    const id = uuid.v4();
+    const outputPath = `output/${id}`;
+
+    // Clone the repo at repoUrl and put it into outputPath
+    await simpleGit().clone(repoUrl, outputPath);
+
+    console.log(repoUrl);
+    res.json({});
+  } else {
+    res.status(400).send({
+      status: "false",
+      message: "Please provide valid repoUrl",
+    });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
